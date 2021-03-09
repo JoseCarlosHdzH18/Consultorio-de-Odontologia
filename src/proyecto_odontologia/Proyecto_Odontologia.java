4d4b58c23c86dd1bfaefc235c05b561053c25359
luis_ <luis_@DESKTOP-MC6JN4S.mshome.net>
2021-03-09 13:14:11 -0600

    prueba 2

--- src/proyecto_odontologia/Proyecto_Odontologia.java
+++ src/proyecto_odontologia/Proyecto_Odontologia.java
@@ -5,6 +5,7 @@ public class Proyecto_Odontologia {
     public static void main(String[] args) {
         System.out.print("Hola como estas");
         // Que onda XD
+        System.out.println("Prueba 2 jajajaja");
     }
     
 }
